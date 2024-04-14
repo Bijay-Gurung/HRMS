93bf8db7477ace259e8bd33ba551febdcb0c1822
Bijay-Gurung <stha4580@gmail.com>
2024-04-14 11:53:36 +0545

    Redirecting the page

---
+++ adminNav.js
@@ -0,0 +1,15 @@
+function dashboard(){
+    location = "adminDashboard.html";
+}
+
+function edm(){
+    location = "adminEmployeeDataManagement.html";
+}
+
+function addNewEmployee(){
+    location = "pageOne.html";
+}
+
+function logout(){
+    location = "login.html";
+}
